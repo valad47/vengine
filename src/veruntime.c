2c6db0b59b160584eeb1025adececcb1b7b7f395
valad47 <valad.racz@gmail.com>
2025-01-06 04:02:11 +0200

    Remove unused stack dunp function

--- src/veruntime.c
+++ src/veruntime.c
@@ -6,31 +6,6 @@
 
 #include <stdio.h>
 
-static void dumpstack (lua_State *L) {
-  int top=lua_gettop(L);
-  for (int i=1; i <= top; i++) {
-    printf("%d\t%s\t", i, luaL_typename(L,i));
-    switch (lua_type(L, i)) {
-      case LUA_TNUMBER:
-        printf("%g\n",lua_tonumber(L,i));
-        break;
-      case LUA_TSTRING:
-        printf("%s\n",lua_tostring(L,i));
-        break;
-      case LUA_TBOOLEAN:
-        printf("%s\n", (lua_toboolean(L, i) ? "true" : "false"));
-        break;
-      case LUA_TNIL:
-        printf("%s\n", "nil");
-        break;
-      default:
-        printf("%p\n",lua_topointer(L,i));
-        break;
-    }
-  }
-}
-
-
 void vengine_runtime_start(vengine_State *L) {
     printf("Starting runtime\n");
     InitWindow(400, 800, "NOTHING");
@@ -44,7 +19,6 @@ void vengine_runtime_start(vengine_State *L) {
             printf("Error during runtime:\n\t%s\n", lua_tostring(L->L, -1));
             exit(1);
         };
-        //dumpstack(L->L);
         if (L->show_fps) {
             DrawFPS(5, 5);
         }
